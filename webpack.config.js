948d87d4811b811cd9b93317c5b9a69a01662df3
Racso1624 <fer10_lb@outlook.com>
2022-06-03 11:59:25 -0600

    Se agrega webpack

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { dirname } = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/, 
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', "@babel/preset-react"],
+            cacheDirectory: true,
+            plugins: ['@babel/plugin-transform-runtime']
+          }
+        }
+      },
+      {
+        test: /\.sass$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(jpeg|png|jpg|svg|gif)$/i,
+        loader: "file-loader",
+        options: {
+          name: "[name].[hash:6].[ext]",
+          outputPath: "images",
+          publicPath: "images",
+          emitFile: true,
+          esModule: false
+        }
+      },
+    ]
+  },
+  plugins:[
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "./src/index.html")
+    })
+  ],
+  devServer: {
+    static: {
+        directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 9000, 
+},
+};
